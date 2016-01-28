26f3d9f45141f98310d75964d66da23f88b2106c
Amit Barve <mtbar131@gmail.com>
2016-01-29 01:12:27 +0530

    Anonymous functions, even? some, filter added

--- Functions.clj
+++ Functions.clj
@@ -100,4 +100,105 @@
            (apply + othernumbers)))
 
 ;;Recursive Functions
+;;===========================
+;A simple recursive function that will keep counting down values until
+;it reaches 0 and print all values that are divisible by 100
+;can be written as follows
+(defn simpleRecursiveCounter [n]
+  (if-not (zero? n)
+    (do
+      (if (= (rem n 100) 0)
+        (println n))
+      (simpleRecursiveCounter (dec n)))))
+;However this function will give 'Stackoverflow' error for any input biggr 
+;than a certain limit
+;Clojure proivdes a clever way to solve this problem. 
+;Clojure provides a 'recur' construct which can be used to for tail recursion
+;recur only allows tail recursion!! But thats the beauty of the function
+;for input of any size it won't overflow the stack
+;Below is example
+(defn newSimpleRecursiveCounter [n]
+  (if-not (zero? n)
+    (do
+      (if (= (rem n 100) 0)
+        (println n))
+      (recur (dec n)))))
+;Notice how we have called the same function again using recur keyword
+;we are not giving explicite call by using function name.
+;recur can only be the last statement of the function otherwise clojure will
+;not complie the code
+
+
+;;Some Interesting higher order functions
+;;=========================================
+
+;every?
+;--------
+;This is a function which will accept one function(a predicate to be more 
+;specific) and a list as an argument. Then it will run that predicate on all
+;members of the list and if predicate returns true for all values then every
+;will return true else will return false
+;Example: Check if all elements in list are even
+(def myNumberList [10, 201, 17, 18, 190])
+(every? even? myNumberList) ;this will return false
+
+;some
+;-----
+;This is same as every except that it will return true even if only one element
+;of the list returns true for given predicate. We can compare every? and some?
+;with AND OR logical operators
+(def studentNamesList ["abc", "xyz", "abcd", "test"])
+(defn equalsMyName [x]
+  (= x "abc"))
+(some equalsMyName studentNamesList)
+;Notice that some doesn't have ? like every?
+
+;filter
+;-------
+;This function accepts a predicate and a list as arguments. Then applies 
+;this predicate on the list and returns a new list of the elements for which 
+;predicate is true.
+;Example
+(defn greaterThan100 [x]
+  (> x 100))
+(def randomSeq [1, 109, 781, 99, 108412, 10, 77])
+(filter greaterThan100 randomSeq) ;Will return all numbers > 100
+
+;memoize
+;-------
+;The famous memoization technique! Yes, clojure has a built in function to
+;do this. This function will cachec the results of previous function calls
+;and whenever a function is called with same arguments it will return cached
+;results
+;Example
+(defn slow-function [n m]
+  (Thread/sleep 1000)
+  (* n m))
+;Above function will accept two arguments, then it will sleep for 1 sec
+;and print the product of two argument. We have deliberately made this 
+;function slow by making it sleep.
+;Lets right a function with memoize
+(def fast-function (memoize slow-function))
+;Lets try running both functions and compare the timings
+;; (time (slow-function 10 20))
+;; (time (fast-function 10 20))
+;; (time (fast-function 10 20))
+;From the output of above three calls we can see that in 3rd call fast-function
+;returns the memoized results. The result that was cached during 2nd call
+
+;;Anonymous function
+;;===================
+;We have seen anonymous functions before. In the explanation of keyword 'defn'
+;we have seen that fn creates a function; this is nothing but an anonymous 
+;function which is later bound to given name with def
+;Example
+(filter (fn [x] (> x 10)) [1, 2, 56 11, 81, 76123])
+;Here we have used an anonymous function as predicate
+;However there is even shorter way for writing anonymous function;
+;Same above function can be written as 
+(filter #(> % 10) [1, 2, 56, 11, 81, 76123])
+;If there are more arguments to anonymous functions then they can be used;
+;by using %1, %2 and so on..
+
+
 
